b1e5c9ba595cf096013935b1a51d9ce571b05d92
Leire M-Berdinos <134960908+leimber@users.noreply.github.com>
2023-11-05 19:37:36 +0000

    fet:footer terminado

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  
+    return (
+
+        <div className="container-fluid bg-dark text-center mt-3 p-1">
+            <span className="text-light"> ✓ Made with ♡ by Majo&Leire </span>
+
+        </div>
+
+    )
+
+    }
+
+export default Footer;
